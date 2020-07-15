8d8789f77ecd9a246a79cbf356e29a5dbe7ed49a
Le Thien <doiden1999@gmail.com>
2020-07-15 21:06:12 +0700

    Change vs2.1 -- Update AccoutFragment

--- app/src/main/java/com/example/hotelbookingapp/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/hotelbookingapp/ui/account/AccountFragment.java
@@ -22,13 +22,13 @@ public class AccountFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         accountViewModel = ViewModelProviders.of(this).get(AccountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_account);
+        /*final TextView textView = root.findViewById(R.id.text_account);
         accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
