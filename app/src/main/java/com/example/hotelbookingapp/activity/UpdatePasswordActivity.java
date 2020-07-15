f2df8ac87580f7259aa1278c407ff4066e455fd5
Le Thien <doiden1999@gmail.com>
2020-07-15 22:37:54 +0700

    Change vs2.3 -- Update AccoutFragment -- Created InformationAccountActivity -- Created UpdateInformationActivity -- Created UpdatePasswordActivity

---
+++ app/src/main/java/com/example/hotelbookingapp/activity/UpdatePasswordActivity.java
@@ -0,0 +1,18 @@
+package com.example.hotelbookingapp.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hotelbookingapp.R;
+
+/**
+ * Created by LeThien on 7/15/2020
+ */
+public class UpdatePasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+    }
+}
